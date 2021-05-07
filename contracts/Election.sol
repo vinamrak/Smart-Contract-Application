d2a11091bfc5eb13df3efb5adc0c6af0ac2e6c68
Vinamra Khoria <Vinamrakhoria@gmail.com>
2021-05-07 11:30:51 +0530

    candidates structure, mapping added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,9 +3,27 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
 
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates; //30:45
+
+    uint public candidatesCount; //32:00
 
     constructor () public {
-            candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+
+
 }
+
